7338b614d0f694369a851f245f822a00e4975893
guilledamian91 <116650602+guilledamian91@users.noreply.github.com>
2023-06-06 08:47:18 -0300

    Add files via upload

---
+++ assets/js/enviarcorreo.js
@@ -0,0 +1,20 @@
+
+function enviarCorreo(event) {
+  event.preventDefault();
+
+  const nombre = document.getElementById('nombre').value;
+  const apellido = document.getElementById('apellido').value;
+  const casa = document.getElementById('casa').value;
+  const mail = document.getElementById('mail').value;
+  const consulta = document.getElementById('consulta').value;
+
+  const link = document.createElement('a');
+  link.href = 'mailto:inmotpo@gmail.com?subject=Nueva consulta de casa&body=Nombre: ' + encodeURIComponent(nombre) + '%0D%0A' + 'Apellido: ' + encodeURIComponent(apellido) + '%0D%0A' + 'Casa a consultar: ' + encodeURIComponent(casa) + '%0D%0A' + 'Mail: ' + encodeURIComponent(mail) + '%0D%0A' + 'Consulta: ' + encodeURIComponent(consulta);
+
+  link.click();
+  document.getElementById('nombre').value = '';
+  document.getElementById('apellido').value = '';
+  document.getElementById('casa').value = '';
+  document.getElementById('mail').value = '';
+  document.getElementById('consulta').value = '';
+}
