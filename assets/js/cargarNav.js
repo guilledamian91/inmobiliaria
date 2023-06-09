27eee98165b1c3114b117b36369117db354b1ca8
jb-2015 <juan_matias_becerra@hotmail.com>
2023-06-09 01:09:36 -0300

    Mensaje

--- assets/js/cargarNav.js
+++ assets/js/cargarNav.js
@@ -8,7 +8,7 @@ window.addEventListener('load',()=>{
 	let who = window.location.pathname
 	
 	if(who=="/inmobiliaria/"||who=="/inmobiliaria/index.html"){
-		c.appendChild(new Nav("logo.png",["Inicio","Comprar","Alquilar","Blog","Franquicia","Contacto"],["./","../pestañas/comprar.html","../pestañas/alquilar.html","../pestañas/blog.html","../pestañas/franquicia.html","../pestañas/contacto.html"]))	
+		c.appendChild(new Nav("logo.png",["Inicio","Comprar","Alquilar","Blog","Franquicia","Contacto"],["./","./pestañas/comprar.html","./pestañas/alquilar.html","./pestañas/blog.html","./pestañas/franquicia.html","./pestañas/contacto.html"]))	
 	}else{
 		c.appendChild(new Nav("../logo.png",["Inicio","Comprar","Alquilar","Blog","Franquicia","Contacto"],["../","../pestañas/comprar.html","../pestañas/alquilar.html","../pestañas/blog.html","../pestañas/franquicia.html","../pestañas/contacto.html"]))
 	}
--- assets/js/classFormulario.js
+++ assets/js/classFormulario.js
@@ -8,7 +8,57 @@ class Formulario{
 
 		this.formulario.addEventListener('submit',e=>{
 			e.preventDefault()
+			let n = this.formulario.elements.nombre.value;
+			let em = this.formulario.elements.email.value;
+			let m= this.formulario.elements.mensaje.value;
+			let flag=false;
+			let errores= document.createElement('ul')
+			errores.style.color= "red"
+			errores.innerHTML=""
+			this.formulario.appendChild(errores);
+			
 
+			if(n==""){
+				let err = document.createElement('li')
+				err.textContent="Algo anda mal en el nombre"
+				errores.appendChild(err)
+				this.formulario.elements.nombre.focus()
+				this.formulario.elements.nombre.classList.toggle('inputHover')
+				flag=true
+			}
+
+			if(em==""){
+				let err = document.createElement('li')
+				err.textContent="Algo anda mal en la direccion de correo"
+				errores.appendChild(err)
+				this.formulario.elements.email.focus()
+				this.formulario.elements.email.classList.toggle('inputHover')
+				flag=true
+			}
+			if(m==""){
+				let err = document.createElement('li')
+				err.textContent="Debe ingresar un mensaje"
+				errores.appendChild(err)
+				this.formulario.elements.mensaje.focus()
+				this.formulario.elements.mensaje.classList.toggle('inputHover')
+				flag=true
+			}
+
+
+
+
+			if(!flag){
+				
+				const link = document.createElement('a');
+				link.href = 'mailto:inmotpo@gmail.com?subject=Nueva consulta de casa&body=Nombre: ' 
+					+ encodeURIComponent(n) 
+					+ '%0D%0A' +'Mail: ' + encodeURIComponent(em) 
+					+ '%0D%0A' + 'Consulta: ' + encodeURIComponent(m);
+
+				link.click();
+				this.formulario.reset()
+				this.formulario.elements.nombre.focus()
+			}
 		})
 
 	}
